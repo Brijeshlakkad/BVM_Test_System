67f78b794b14c4024dfa74377d567eee38aba142
Brijeshlakkad <Brijeshlakkad@github.com>
2018-10-10 14:54:02 +0530

    created student profile page

---
+++ profile.php
@@ -0,0 +1,10 @@
+<?php
+include_once("header.php");
+include_once("functions.php");
+check_session();
+check_pages();
+?>
+<div class="container my_well_1">
+
+</div>
+<?php include_once("footer.php"); ?>
