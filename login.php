ecc7f3acb0169911eb458acb011f04cad7808a00
Brijeshlakkad <Brijeshlakkad@github.com>
2018-10-11 17:10:29 +0530

    Faculty and student login both added in login page

--- login.php
+++ login.php
@@ -1,32 +1,8 @@
 <?php
 include_once("functions.php");
 check_pages();
+include_once("links.php");
 ?>
-<!DOCTYPE html>
-<html lang="en" class="no-js">
-	<head>
-		<meta charset="UTF-8" />
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>BVM Test System</title>
-		<meta name="description" content="" />
-		<meta name="keywords" content="" />
-		<meta name="author" content="Brijesh Lakkad" />
-		<link rel="stylesheet" type="text/css" href="css/normalize.css" />
-		<link rel="stylesheet" type="text/css" href="css/gn_demo.css" />
-		<link rel="stylesheet" type="text/css" href="css/component.css" />
-		<script src="js/modernizr.custom.js"></script>
-
-		<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-		<link href="css/custom2.css" rel="stylesheet" media="screen">
-		<link rel="stylesheet" href="css/font-awesome.min.css">
-		<link rel="stylesheet" type="text/css" href="css/please_wait.css" />
-		<script src="js/jquery.min.js"></script>
-		<script src="js/bootstrap.min.js"></script>
-		<script src="js/angular.js"></script>
-		<script type="text/javascript" src="js/bootstrap-show-password.min.js"></script>
-	</head>
-	<body>
 <div class="container well login_block" align="center">
 	<div class="row">
 	<div class="gn_container" style="margin-top:45px;"><caption>
@@ -37,9 +13,18 @@ check_pages();
 	</div>
 	<div class="row">
 		<form ng-app="myapp" ng-controller="BrijController" name="myForm" method="post" novalidate>
-		<div class="row" style="padding-top: 20px;"><div class="col-md-offset-4 col-md-4 col-md-offset-4"><div id="l_status"></div></div></div>
+		<div class="row" style="padding-top: 20px;"><div class="col-md-offset-4 col-md-4 col-md-offset-4"><div ng-show="l_status_0" class="alert alert-danger">Email or password is wrong</div><div ng-show="l_status_1" class="alert alert-success">Login in..</div></div></div>
 		<table class="myTable">
 			<div class="form-group">
+				<tr>
+				  <td><label for="l_type">Login Type</label></td>
+				  <td><select class="form-control" name="l_type" ng-model="l_type">
+						<option value="f_">Faculty</option>
+						<option value="student_">Student</option>
+					</select>
+					</td>
+				  <td></td>
+				</tr>
 			<tr>
 				<td><label for="l_email">Email:</label></td>
 				<td><input type="email" class="form-control" name="l_email" placeholder="Enter Email" ng-model="l_email" ng-style="emailStyle" ng-change="analyze2(l_email)" required /></td>
@@ -64,7 +49,7 @@ check_pages();
 			<tr>
 				<td></td>
 				<td>
-					<span style="color:red" id="l_password" ng-show="myForm.l_password.$dirty && myForm.l_password.$invalid">
+					<span style="color:red;" id="l_password" ng-show="myForm.l_password.$dirty && myForm.l_password.$invalid">
 					<span ng-show="myForm.l_password.$error.required">Password is required.</span>
 					</span>
 				</td>
@@ -73,7 +58,7 @@ check_pages();
 			</div>
 			<tr>
 
-				<td><input type="submit" class="btn btn-primary" onclick="return login_status()" ng-disabled="myForm.l_email.$invalid || myForm.l_password.$invalid"></td>
+				<td><input type="submit" class="btn btn-primary" ng-click="login_status()" ng-disabled="myForm.l_email.$invalid || myForm.l_password.$invalid || myForm.l_type.$invalid"></td>
 				<td></td>
 				<td></td>
 
@@ -102,7 +87,13 @@ check_pages();
 <script>
 	var myApp = angular.module("myapp", []);
 	myApp.controller("BrijController", function($scope,$http) {
-
+				$scope.l_type="f_";
+				$scope.addCustomClass = function (ele_id,c_name) {
+                angular.element(document.querySelector("#"+ele_id)).addClass(""+c_name);
+            };
+            $scope.removeCustomClass = function (ele_id,c_name) {
+                angular.element(document.querySelector("#"+ele_id)).removeClass(""+c_name);
+            };
 				$scope.emailStyle = {
 					"border-width":"1.45px"
                 };
@@ -115,43 +106,31 @@ check_pages();
                         $scope.emailStyle["border-color"] = "red";
                     }
                 };
+					$scope.login_status =function()
+									{
+										$http({
+													method : "POST",
+													url : "login_data.php",
+													data: "l_type="+$scope.l_type+"&l_email="+$scope.l_email+"&l_password="+$scope.l_password,
+													headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+												}).then(function mySuccess(response) {
+													flag = response.data;
+													// we should be using flag in only this block so logic in following
+													if(flag!=0)
+													{
+														$scope.l_status_0=false;
+														$scope.l_status_1=true;
+														document.location="profile.php";
+													}
+													else
+													{
+														$scope.l_status_1=false;
+														$scope.l_status_0=true;
+													}
+												}, function myError(response) {
 
+												});
+									};
 });
-
-function login_status()
-	{
-		var email =	document.myForm.l_email.value;
-		var pass =	document.myForm.l_password.value;
-		var l_email=document.getElementById('l_email').innerHTML;
-		var l_pass=document.getElementById('l_password').innerHTML;
-		if(l_email!="Email is required." && l_email!="Invalid email address." && l_pass!="Password is required.")
-			{
-				var x=new XMLHttpRequest();
-				x.onreadystatechange=function()
-				{
-					if(x.readyState==4 && x.status==200)
-						{
-							var data=this.responseText;
-							if(data!=0)
-							{
-								$("#l_status").removeClass("alert alert-danger").addClass("alert alert-success").html("Logging in....");
-								document.location="profile.php";
-							}
-							else
-							{
-								$("#l_status").removeClass("alert alert-success").addClass("alert alert-danger").html("Email or password is wrong");
-							}
-						}
-				};
-				x.open("POST","login_data.php",true);
-				x.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-				x.send("l_email="+email+"&l_password="+pass);
-			}
-		else
-		{
-			document.getElementById('l_status').innerHTML="<p style='color:red;'>please enter details properly</p>"
-		}
-		return;
-	}
 </script>
 <?php include("footer.php"); ?>
--- login_data.php
+++ login_data.php
@@ -1,9 +1,19 @@
 <?php
 include_once("functions.php");
 include_once("config.php");
+$type=protect_anything($_POST['l_type']);
+if($type=="f_")
+{
+	$table_name="faculty";
+}
+else {
+	$table_name="students";
+}
+$email_field=$type."email";
+$password_field=$type."password";
 $email=protect_anything($_POST['l_email']);
 $password=protect_anything($_POST['l_password']);
-$sql="SELECT * FROM students WHERE student_email='$email' AND student_password='$password'";
+$sql="SELECT * FROM $table_name WHERE $email_field='$email' AND $password_field='$password'";
 $result=mysqli_query($con,$sql);
 $r=mysqli_num_rows($result);
 $row=mysqli_fetch_assoc($result);
